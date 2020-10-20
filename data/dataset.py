c1cf0d5c6ab6ef7b583a4c56d3e4ed84eec07aae
Amrit Saha <amritsaha607@gmail.com>
2020-10-20 16:33:50 +0530

    model-init

--- data/dataset.py
+++ data/dataset.py
@@ -23,14 +23,10 @@ class AreaDataset(Dataset):
         self.files = []
         for format_ in formats:
             self.files += glob.glob(os.path.join(root, '*', '*{}'.format(format_)))
-
-        # print(self.files)
-    
+   
     def __getitem__(self, index):
         file = self.files[index]
         x, y = extractData(file)
-        # x = torch.tensor(x)
-        # y = torch.tensor(y)
         return x, y
 
     def __len__(self):
