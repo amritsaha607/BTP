9f2e15ec4b292b69ee931b4c180920011cf8ba2b
Amrit Saha <amritsaha607@gmail.com>
2020-10-21 01:08:26 +0530

    added-normalization-factors

--- data/utils.py
+++ data/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 
-def extractData(filename):
+def extractData(filename, factors={'r': 1e9, 'eps': 1e7, 'lambd': 1e9, 'A': 1e17}):
     '''
         Extracts data from a csv file for training
         Args:
@@ -11,6 +11,11 @@ def extractData(filename):
                         [supported formats => '.csv']
     '''
 
+    if not factors:
+        f_r, f_eps, f_lambd, f_A = 1, 1, 1, 1
+    else:
+        f_r, f_eps, f_lambd, f_A = factors['r'], factors['eps'], factors['lambd'], factors['A']
+    
     df = pd.read_csv(filename)
 
     # Input contains two columns, wavelength and area
@@ -18,18 +23,18 @@ def extractData(filename):
 
     # Input data is a list of combined wavelength and area
     # First all wavelength data followed by area data
-    x = np.concatenate([df['lambd'].values, df['A_tot'].values], axis=0)
+    x = np.concatenate([f_lambd*df['lambd'].values, f_A*df['A_tot'].values], axis=0)
 
     # Output contains parameters like r1, r2, eps_1, eps_2 & eps_3
     y = np.array([
-        df['r1'][0],
-        df['r2'][0],
-        complex(df['eps_1'][0]).real,
-        complex(df['eps_1'][0]).imag,
-        complex(df['eps_2'][0]).real,
-        complex(df['eps_2'][0]).imag,
-        complex(df['eps_3'][0]).real,
-        complex(df['eps_3'][0]).imag
+        f_r*df['r1'][0],
+        f_r*df['r2'][0],
+        f_eps*complex(df['eps_1'][0]).real,
+        f_eps*complex(df['eps_1'][0]).imag,
+        f_eps*complex(df['eps_2'][0]).real,
+        f_eps*complex(df['eps_2'][0]).imag,
+        f_eps*complex(df['eps_3'][0]).real,
+        f_eps*complex(df['eps_3'][0]).imag
     ])
 
     return x, y
