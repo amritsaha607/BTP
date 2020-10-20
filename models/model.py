7cf1c36511c313520be82e7dd357c975d6e03b04
Amrit Saha <amritsaha607@gmail.com>
2020-10-21 01:28:08 +0530

    temporary-changes

--- models/model.py
+++ models/model.py
@@ -7,14 +7,21 @@ class BasicModel(nn.Module):
     def __init__(self, input_dim=98):
         super(BasicModel, self).__init__()
 
-        self.lin1 = nn.Linear(input_dim, 100)
-        self.relu1 = nn.ReLU()
-        self.lin2 = nn.Linear(100, 8)
-        self.relu2 = nn.ReLU()
+        self.linear1 = nn.Linear(input_dim, 100)
+        self.relu1 = nn.LeakyReLU()
+        self.linear2 = nn.Linear(100, 64)
+        self.relu2 = nn.LeakyReLU()
+        self.linear3 = nn.Linear(64, 32)
+        self.relu3 = nn.ReLU()
+        self.linear4 = nn.Linear(32, 8)
+        # self.relu3 = nn.ReLU()
 
     def forward(self, x):
-        y = self.lin1(x)
+        y = self.linear1(x)
         y = self.relu1(y)
-        y = self.lin2(y)
+        y = self.linear2(y)
         y = self.relu2(y)
+        y = self.linear3(y)
+        y = self.relu3(y)
+        y = self.linear4(y)
         return y
