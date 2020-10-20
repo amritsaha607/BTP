e8c08b6ff4c782231b0f9531fc49d10310381de2
Amrit Saha <amritsaha607@gmail.com>
2020-10-20 15:39:07 +0530

    added-dataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,13 @@
+import os
+import glob
+import pandas as pd
 import numpy as np
+import torch
 from torch.utils.data import Dataset
 import random
+
+from data.utils import extractData
+from utils.decorators import timer
 
 
 class AreaDataset(Dataset):
@@ -10,7 +17,21 @@ class AreaDataset(Dataset):
         and the parameters used for the same calculation
     """
 
-    def __init__(self):
+    def __init__(self, root='dataGeneration/data/', formats=['.csv']):
         super(AreaDataset, self).__init__()
 
+        self.files = []
+        for format_ in formats:
+            self.files += glob.glob(os.path.join(root, '*', '*{}'.format(format_)))
+
+        # print(self.files)
     
+    def __getitem__(self, index):
+        file = self.files[index]
+        x, y = extractData(file)
+        # x = torch.tensor(x)
+        # y = torch.tensor(y)
+        return x, y
+
+    def __len__(self):
+        return len(self.files)
---
+++ data/utils.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+def extractData(filename):
+    '''
+        Extracts data from a csv file for training
+        Args:
+            filename    : filename to extract data from
+                        [supported formats => '.csv']
+    '''
+
+    df = pd.read_csv(filename)
+
+    # Input contains two columns, wavelength and area
+    x = np.c_[df['lambd'], df['A_tot']]
+
+    # Output contains parameters like r1, r2, eps_1, eps_2 & eps_3
+    y = [
+        df['r1'][0],
+        df['r2'][0],
+        complex(df['eps_1'][0]).real,
+        complex(df['eps_1'][0]).imag,
+        complex(df['eps_2'][0]).real,
+        complex(df['eps_2'][0]).imag,
+        complex(df['eps_3'][0]).real,
+        complex(df['eps_3'][0]).imag
+    ]
+
+    return x, y
