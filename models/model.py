97da09a61e0b402d2c859e2f1f88cb15eda1c950
Amrit Saha <amritsaha607@gmail.com>
2020-11-02 11:40:50 +0530

    temp-changes

--- models/model.py
+++ models/model.py
@@ -9,7 +9,7 @@ class BasicModel(nn.Module):
         super(BasicModel, self).__init__()
 
         self.size = 1024
-        self.n_layers_1, self.n_layers_2 = 3, 5
+        self.n_layers = 5
 
         size = self.size
 
@@ -17,33 +17,31 @@ class BasicModel(nn.Module):
         
         self.layer1 = DeepLayer(
             size=size,
-            n_layers=self.n_layers_1,
-            d_factor=1,
-            activation='relu',
-            bn=False
-        )
-        self.layer2 = DeepLayer(
-            size=size,
-            n_layers=self.n_layers_2,
+            n_layers=self.n_layers,
             d_factor=2,
             activation='relu',
             bn=False
         )
 
-        self.out = nn.Linear(size//(2**self.n_layers_2), out_dim)
+        size = size//(2**self.n_layers)
+        self.layer2 = DeepLayer(
+            size=size,
+            n_layers=self.n_layers,
+            d_factor=1/2,
+            activation='relu',
+            bn=False
+        )
+        size = int(size//((1/2)**self.n_layers))
+
+        self.out = nn.Linear(size, out_dim)
 
     def cuda(self, *args, **kwargs):
         super(BasicModel, self).cuda()
-        # self.layer1 = self.layer1.cuda()
-        # self.layer2 = self.layer2.cuda()
 
     def forward(self, x):
-
-        y = self.in_(x)
-        y = self.layer1(y)
+        y1 = self.in_(x)
+        y = self.layer1(y1)
         y = self.layer2(y)
-        # y = self.layer1_1(y) + self.layer1_2(y)
-        # y = self.layer2_1(y) + self.layer2_2(y)
-        # y = self.layer(y)
+        y = y + y1
         y = self.out(y)
         return y
