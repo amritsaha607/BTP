5efae58fd3a04d063e54a2cf664ce55cc7abaafd
Amrit Saha <amritsaha607@gmail.com>
2020-11-07 13:02:18 +0530

    implemented-model-9

--- models/model.py
+++ models/model.py
@@ -92,6 +92,71 @@ class BasicModel(nn.Module):
 
             self.out = nn.Linear(size, out_dim)
 
+        elif model_id==9:
+            self.size = 1024
+            self.n_layers = [3, 5, 5, 5, 5, 3]
+            self.d_factors = [2, 1, 1, 1, 1, 1/2]
+
+            size = self.size
+
+            self.in_ = Dense(input_dim, self.size)
+
+            self.layer1 = DeepLayer(
+                size=size,
+                n_layers=self.n_layers[0],
+                d_factor=self.d_factors[0],
+                activation='relu',
+                bn=False
+            )
+            size = int(size // (self.d_factors[0]**self.n_layers[0]))
+
+            self.layer2 = DeepLayer(
+                size=size,
+                n_layers=self.n_layers[1],
+                d_factor=self.d_factors[1],
+                activation='relu',
+                bn=False
+            )
+            size = int(size // (self.d_factors[1]**self.n_layers[1]))
+
+            self.layer3 = DeepLayer(
+                size=size,
+                n_layers=self.n_layers[2],
+                d_factor=self.d_factors[2],
+                activation='relu',
+                bn=False
+            )
+            size = int(size // (self.d_factors[2]**self.n_layers[2]))
+
+            self.layer4 = DeepLayer(
+                size=size,
+                n_layers=self.n_layers[3],
+                d_factor=self.d_factors[3],
+                activation='relu',
+                bn=False
+            )
+            size = int(size // (self.d_factors[3]**self.n_layers[3]))
+
+            self.layer5 = DeepLayer(
+                size=size,
+                n_layers=self.n_layers[4],
+                d_factor=self.d_factors[4],
+                activation='relu',
+                bn=False
+            )
+            size = int(size // (self.d_factors[4]**self.n_layers[4]))
+
+            self.layer6 = DeepLayer(
+                size=size,
+                n_layers=self.n_layers[5],
+                d_factor=self.d_factors[5],
+                activation='relu',
+                bn=False
+            )
+            size = int(size // (self.d_factors[5]**self.n_layers[5]))
+
+            self.out = nn.Linear(size, out_dim)
+
     def cuda(self, *args, **kwargs):
         super(BasicModel, self).cuda()
 
@@ -109,6 +174,16 @@ class BasicModel(nn.Module):
             y = self.layer1(y)
             y = self.layer2_1(y) + self.layer2_2(y)
             y = self.layer3(y)
+            y = self.out(y)
+
+        elif self.model_id==9:
+            y = self.in_(x)
+            y = self.layer1(y)
+            y = self.layer2(y) + y
+            y = self.layer3(y) + y
+            y = self.layer4(y) + y
+            y = self.layer5(y) + y
+            y = self.layer6(y)
             y = self.out(y)
 
         return y
