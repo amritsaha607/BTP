44965b55c75b1087fe31269a2a86116a37767de9
Amrit Saha <amritsaha607@gmail.com>
2021-01-28 21:12:00 +0530

    syntax-fixes

--- data/utils.py
+++ data/utils.py
@@ -69,25 +69,27 @@ class PermittivityCalculator:
         """
         if element == "sio2":
             # https://refractiveindex.info/?shelf=main&book=SiO2&page=Radhakrishnan-o
-            n_sq = 1 + 0.663044*(lambd**2)/(lambd**2-0.060**2) + 
-                0.517852*(lambd**2)/(lambd**2-0.106**2) + 0.175912*(lambd**2)/(lambd**2-0.119**2) + 
+            n_sq = 1 + 0.663044*(lambd**2)/(lambd**2-0.060**2) + \
+                0.517852*(lambd**2)/(lambd**2-0.106**2) + 0.175912*(lambd**2)/(lambd**2-0.119**2) + \
                 0.565380*(lambd**2)/(lambd**2-8.844**2) + 1.675299*(lambd**2)/(lambd**2-20.742**2)
             k_sq = 0.0
 
         elif element == "al2o3":
             # https://refractiveindex.info/?shelf=main&book=Al2O3&page=Malitson-o
-            n_sq = 1 + 1.4313493*(lambd**2)/(lambd**2-(0.0726631**2)) + 0.65054713*(lambd**2)/(lambd**2-(0.1193242**2)) + 5.3414021*(lambd**2)/(lambd**2-(18.028251**2))
+            n_sq = 1 + 1.4313493*(lambd**2)/(lambd**2-(0.0726631**2)) + \
+                0.65054713*(lambd**2)/(lambd**2-(0.1193242**2)) + \
+                5.3414021*(lambd**2)/(lambd**2-(18.028251**2))
             k_sq = 0.0
 
         elif element == "hgs":
             # https://refractiveindex.info/?shelf=main&book=HgS&page=Bond-o
-            n_sq = 1 + 3.1506 + 2.7896*(lambd**2)/(lambd**2-0.1328) + 
+            n_sq = 1 + 3.1506 + 2.7896*(lambd**2)/(lambd**2-0.1328) + \
                 1.1378*(lambd**2)/(lambd**2-1705)
             k_sq = 0.0
 
         elif element == "aggas2":
             # https://refractiveindex.info/?shelf=main&book=AgGaS2&page=Takaoka-o
-            n_sq = 5.7975 + 0.2311/(lambd**2-0.0688) -
+            n_sq = 5.7975 + 0.2311/(lambd**2-0.0688) - \
                 0.00257*(lambd**2)
             k_sq = 0.0
         
